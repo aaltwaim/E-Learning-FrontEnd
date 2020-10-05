6ca92f15466078a9e483789fd4cf9f734c571828
aaltwaim <a_t1411@hotmail.com>
2020-10-05 18:46:02 +0300

    Creating home page, other components, and adding a theme to home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { StudentComponent } from './user/student/student.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { LoginComponent } from './user/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component';
+import { CourseComponent } from './category/course/course.component';
+import { SectionComponent } from './category/course/section/section.component';
+import { VideoComponent } from './category/course/section/video/video.component';
+import { StatusComponent } from './category/course/status/status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    StudentComponent,
+    RegistrationComponent,
+    LoginComponent,
+    HomeComponent,
+    CategoryComponent,
+    CourseComponent,
+    SectionComponent,
+    VideoComponent,
+    StatusComponent
   ],
   imports: [
     BrowserModule,
